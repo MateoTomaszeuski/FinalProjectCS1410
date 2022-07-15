f19aeb9cf929560bb510324634a95f1a91bc1abc
Mateo Tomaszeuski <mtomaszeuski@gmail.com>
2022-07-15 15:57:59 -0600

    Play Pause Button

---
+++ ClassLib/IButton.cs
@@ -0,0 +1,4 @@
+﻿public interface IButton
+{
+    public Action GetAction();
+}
---
+++ ClassLib/PlayButton.cs
@@ -0,0 +1,22 @@
+﻿public class PlayPauseButton : IButton
+{
+    private Action Input { get; set; }
+    private bool IsPlaying { get; set; }
+    public PlayPauseButton(Action input)
+    {
+        Input = input;
+        if(IsPlaying)
+        {
+            IsPlaying = false;
+        }
+        else
+        {
+            IsPlaying = true;
+        }
+    }
+    
+    public Action GetAction()
+    {
+        return Input;
+    }
+}
