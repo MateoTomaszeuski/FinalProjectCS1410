71be828c487235c682bae711e1303f47d96ce6c9
Mateo Tomaszeuski <mtomaszeuski@gmail.com>
2022-07-27 20:13:23 -0600

    next song method

--- ClassLib/Song.cs
+++ ClassLib/Song.cs
@@ -9,7 +9,7 @@ public record Song
         Name = name;
         SongPath = path;
     }
-    public static void AddSongsToList(string DirectorySongs, ref List<Song> SongList )
+    public static void AddSongsToList(string DirectorySongs, ref List<Song> SongList)
     {
         SongList.Clear();
         SongList.Add(new Song("", ""));
@@ -30,14 +30,37 @@ public record Song
             SongList.Add(songNameWithoutExtension);
         }
     }
-    
-    public static void GetNameandPath(ref string SelectedSong ,ref string[]? nameAndPath,ref string path, ref string Songname )
+
+    public static void GetNameandPath(ref string SelectedSong, ref string[]? nameAndPath, ref string path, ref string Songname)
     {
-        
+
         nameAndPath = SelectedSong?.Split(",");
         path = nameAndPath?[0];
         path = "/Songs/" + path?.Remove(0, 14) + ".mp3";
         Songname = path.Remove(0, 7);
         Songname = Songname.Remove(Songname.Length - 4);
     }
+    public static void nextSong(List<Song> SongList, string Songname, int songpositon, ref string selectedSong, ref string[]? nameAndPath, ref string temppath, ref string songname)
+    {
+        for (int i = 0; i < SongList.Count; i++)
+        {
+
+            if (Songname == SongList[i].Name)
+            {
+                songpositon = i;
+                break;
+            }
+        }
+        if (songpositon + 2 > SongList.Count)
+        {
+            selectedSong = SongList[1].ToString();
+        }
+        else
+        {
+            selectedSong = SongList[songpositon + 1].ToString();
+
+        }
+        GetNameandPath(ref selectedSong!, ref nameAndPath!, ref temppath, ref songname);
+    }
+
 }
